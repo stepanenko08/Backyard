5fe8c6ec56a5796537f72017b37787770c767479
Stepanenko Vladyslav <111245894+stepanenko08@users.noreply.github.com>
2022-12-26 22:53:44 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+
+  $('.slider').slick({
+    // autoplay:true,
+    // autoplaySpeed:1000,
+    // // arrows:false,
+    // prevArrow:'<p class="custom__arrow">5555555</p>',
+    // dots:true,
+
+  });
+
+  new WOW().init();
+
+})
