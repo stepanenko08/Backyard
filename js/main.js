2551f56bc0eb1bfe76fadff37d7cdfc2faa44fd3
Stepanenko Vladyslav <stepanenkovlad2008@gmail.com>
2022-12-29 22:06:23 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $( document ).ready(function() {
 
   $('.slider').slick({
-    // autoplay:true,
-    // autoplaySpeed:1000,
+    autoplay:true,
+    autoplaySpeed:2000,
     // // arrows:false,
     // prevArrow:'<p class="custom__arrow">5555555</p>',
     // dots:true,
